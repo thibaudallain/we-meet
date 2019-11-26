6d99738f479976d474e73f527b6942881c643637
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-26 18:12:36 +0100

    bug js fixed in share.js

--- app/javascript/components/share.js
+++ app/javascript/components/share.js
@@ -1,24 +1,26 @@
 const share = () => {
 const btn = document.querySelector('.share');
-btn.addEventListener('click', (event) => {
+if (btn) {
+  btn.addEventListener('click', (event) => {
 
-    const eventId = btn.dataset.event;
-    console.log(eventId);
-  if (navigator.share) {
-    console.log("it's working");
-    navigator.share({
-      title: "We Meet",
-      text: "We Meet - Invitation pour ce soir",
-      url: `/events/${eventId}/share`
-    }).then(() => {
-      console.log("Link shared");
-    })
-    .catch(console.error);
-  } else {
-    console.log("Link was not shared");
-  };
+      const eventId = btn.dataset.event;
+      console.log(eventId);
+    if (navigator.share) {
+      console.log("it's working");
+      navigator.share({
+        title: "We Meet",
+        text: "We Meet - Invitation pour ce soir",
+        url: `/events/${eventId}/share`
+      }).then(() => {
+        console.log("Link shared");
+      })
+      .catch(console.error);
+    } else {
+      console.log("Link was not shared");
+    };
 
-});
+  });
+}
 };
 
 export { share };
