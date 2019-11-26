6ee272f2532905c2f584550bef171dad5c0e9046
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-26 12:44:42 +0100

    google api ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
+require_relative "../services/google_places"
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @bars = retrieve_bars
+  end
+
+  private
+
+  def retrieve_bars
+    @key = ENV['GOOGLE_PLACES_API_KEY']
+    GooglePlacesServices.new(location: "48.870299,2.375993", key: @key).places_api_call
   end
 end
