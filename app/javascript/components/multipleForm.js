9d5d5cabd90fba7e18b104cf430273812aaeb3d1
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-26 16:53:29 +0100

    modify onglet

--- app/javascript/components/multipleForm.js
+++ app/javascript/components/multipleForm.js
@@ -3,18 +3,28 @@ const multipleForm = () => {
   const step2 = document.querySelector(".step-2");
   const formUser = document.getElementById("form-user");
   const formAvailability = document.getElementById("form-availability");
-  step1.addEventListener("click", (event) => {
+  const next = document.querySelector(".next");
+  step1.addEventListener("", (event) => {
     formUser.classList.remove("d-none");
     formAvailability.classList.add("d-none");
     step2.classList.remove("active");
     step1.classList.add("active");
   });
-  step2.addEventListener("click", (event) => {
+  step2.addEventListener("", (event) => {
+    formAvailability.classList.remove("d-none");
+    formUser.classList.add("d-none");
+    step1.classList.remove("active");
+    step2.classList.add("active");
+
+  });
+next.addEventListener("click", (event) => {
     formAvailability.classList.remove("d-none");
     formUser.classList.add("d-none");
     step1.classList.remove("active");
     step2.classList.add("active");
   });
 };
+
+
 
 export {multipleForm};
