28fcbe2fd966dc6617a59234b1792cb61eb05ac7
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-26 13:14:56 +0100

    form new ok for input name, address and phone stil some scss

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events, only: [:new] do
+  resources :events, only: [:new, :create] do
     get "/share", to: "events#share"
   end
 end
