a93e540344d0e49b7b2ab3e160c53a5be09cb440
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-27 14:53:44 +0100

    form modified with form tag instead of simple form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,14 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+#   def create
+#     @event = Event.new(event_params)
+      #@user = params[:user_id]
+#     @event.save
+#   end
+
+#   def event_params
+#     params.require(:event).permit()
+#   end
 end
