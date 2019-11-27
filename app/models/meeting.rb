babb3f6592dfa6e8c222a52a4c4c42e9a5b9833f
Adelaide Blot <adelaide.blot@hotmail.fr>
2019-11-27 11:56:16 +0100

    routes et controllers

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,11 +1,14 @@
 class Meeting < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :address, presence: true
-  validates :event_id, presence: true
-  validates :user_id, presence: true
+  # validates :event_id, presence: true
+  # validates :user_id, presence: true
+
+
   validates_inclusion_of :organizer, in: [true, false]
-  validates_inclusion_of :attending, in: [true, false]
+  # validates_inclusion_of :attending, in: [true, false]
+  validates :attending, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
