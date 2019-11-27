6ec6328bf6f3e96b6e212447b5d33afd6f460f7d
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-27 18:14:20 +0100

    better seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,83 +5,78 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+User.destroy_all
+Meeting.destroy_all
 
-Event.create(
+event = Event.create(
   date: Date.today,
   start_time: Time.now + 3700,
   deadline: Time.now + 1800,
   )
 
-User.create(
-  name: "Jean",
-  email: "jean@lewagon.com",
-  password: "123456",
+thibaud = User.create(
+  name: "Thibaud",
   phone_number: "0627801824"
   )
 
-User.create(
+michel = User.create(
   name: "Michel",
-  email: "michel@lewagon.com",
-  password: "123456",
   phone_number: "0627801825"
   )
 
-User.create(
+robert = User.create(
   name: "Robert",
   email: "robert@lewagon.com",
   password: "123456",
   phone_number: "0627801826"
   )
 
-User.create(
+daniel = User.create(
   name: "Daniel",
-  email: "daniel@lewagon.com",
-  password: "123456",
   phone_number: "0627801827"
   )
 
-User.create(
+atchoum = User.create(
   name: "Atchoum",
-  email: "atchoum@lewagon.com",
-  password: "123456",
   phone_number: "0627801828"
   )
 
 Meeting.create(
-  event_id: 1,
-  user_id: 1,
+  event_id: event,
+  user_id: thibaud,
   attending: true,
   address: "11 rue de l'Orillon, 75011",
   organizer: true
   )
 
 Meeting.create(
-  event_id: 1,
-  user_id: 2,
+  event_id: event,
+  user_id: michel,
   attending: true,
   address: "11 rue de Rivoli, 75001",
   organizer: true
   )
 
 Meeting.create(
-  event_id: 1,
-  user_id: 3,
+  event_id: event,
+  user_id: robert,
   attending: false,
   address: "67 boulevard de la Villette, 75020",
   organizer: false
   )
 
 Meeting.create(
-  event_id: 1,
-  user_id: 4,
+  event_id: event,
+  user_id: daniel,
   attending: true,
   address: "210 boulevard voltaire, 75011",
   organizer: false
   )
 
 Meeting.create(
-  event_id: 1,
-  user_id: 5,
+  event_id: event,
+  user_id: atchoum,
   attending: true,
   address: "1 rue de charonne, 75011",
   organizer: false
