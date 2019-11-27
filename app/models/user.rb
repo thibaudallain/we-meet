01e39d11d60cb0f69654b48c55d749248b3e2c77
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-27 12:22:40 +0100

    class container modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # validates :name, :phone_number, :address, presence: :true
+
   has_many :meetings
 end
