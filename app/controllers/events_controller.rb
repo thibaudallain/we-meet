5f6a0d01d42963ee5c22b12ceebc7899d431fd0f
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-27 16:41:10 +0100

    form rendre to share events ok and user create and event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  # before_action :authenticate_user!
   def share
   end
 
@@ -6,13 +7,22 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-#   def create
-#     @event = Event.new(event_params)
-      #@user = params[:user_id]
-#     @event.save
-#   end
+  def create
+    @user = User.create(name: params[:name], phone_number: params[:phone_number])
+    @user.save
+    @event = Event.new(event_params)
+    if @event.save
+      # meeting = Meeting.create(event: @event)
+      # meeting.save
+      redirect_to event_share_path(@event)
+    else
+      redirect_to root_path
+    end
+  end
 
-#   def event_params
-#     params.require(:event).permit()
-#   end
+  private
+
+  def event_params
+    params.require(:event).permit(:start_time, :deadline)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   has_many :suggested_bars
   has_many :meetings
   has_many :users, through: :meetings
-  validates :date, presence: true
+  # validates :date, presence: true
   validates :start_time, presence: true
   validates :deadline, presence: true
   validate :deadline_is_before_start_time
