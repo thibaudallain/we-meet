ff9bd601321e42922f8e868befba2943d4bbb244
Sergio Delgado <sergio@dgtl.ec>
2019-11-28 17:52:47 +0100

    front

--- app/javascript/components/loader.js
+++
@@ -1,6 +0,0 @@
-const loader = () => {
-
-
-}
-
-export { loader }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import { share } from "../components/share";
 import { multipleForm } from "../components/multipleForm";
-import { loader } from "../components/loader";
 import { vote } from "../components/vote";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from "../plugins/init_mapbox"
