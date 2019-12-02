22fc2a7079b4ece0287f184dc7f0c4d64fee9fd9
Adelaide Blot <adelaide.blot@hotmail.fr>
2019-12-02 15:43:31 +0100

    debug

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .json
 //= link application.css
-//= link manifest.json
