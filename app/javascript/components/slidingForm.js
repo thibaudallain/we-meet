eef400bd69b980a2c6709df65f496ba883d44f39
Adelaide Blot <adelaide.blot@hotmail.fr>
2019-12-03 18:35:09 +0100

    bugfix

--- app/javascript/components/slidingForm.js
+++ app/javascript/components/slidingForm.js
@@ -1,28 +1,8 @@
-const able = () => {
-  const name = document.getElementById('name');
-  const phone = document.getElementById('phone_number');
-  const start = document.getElementById("event_start_time");
-  const btn = document.getElementById('new-event');
-  const address = document.getElementById('address');
-  let fir = name.classList.contains('valid-input');
-  let sec = phone.classList.contains('valid-input');
-  let thi = start.classList.contains('valid-input');
-  let fou = start.classList.contains('valid-input');
-  if (fir && sec && thi && fou) {
-    btn.disabled = false;
-    btn.classList.remove('btn-disabled');
-  } else {
-    btn.disabled = true;
-    btn.classList.add('btn-disabled');
-  };
-}
-
 const slideForm = () => {
   const firstArrow = document.querySelector('#first-arrow');
   const secondPart = document.getElementById('form-2');
     firstArrow.addEventListener('click', (event) => {
       secondPart.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-      able();
     });
 
   const secondArrow = document.querySelector('#second-arrow');
