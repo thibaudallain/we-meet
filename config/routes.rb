823e05475802ded1a3827850fd529c4a9e39eeda
Thibaud Allain <thibaudallainta@gmail.com>
2019-12-03 12:05:26 +0100

    action cable config done

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     resources :suggested_bars, only: [:index]
     get "/suggested_bars/login", to: "suggested_bars#login"
   end
+  mount ActionCable.server => "/cable"
 end
