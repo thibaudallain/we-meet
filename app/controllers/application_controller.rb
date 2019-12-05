6b992125c04e954f8c9df72f1ec0d4e547569abd
Tanguy <blondiaux.tanguy@gmail.com>
2019-12-05 15:23:58 +0100

    title and meta in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
